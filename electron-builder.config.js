1963928f2b1bb7e8a844aa520bf4b705e13bce34
Vadko <vpoglid@gmail.com>
2025-11-27 04:57:52 +0200

    update repo visibility

---
+++ electron-builder.config.js
@@ -0,0 +1,57 @@
+/**
+ * @type {import('electron-builder').Configuration}
+ * @see https://www.electron.build/configuration/configuration
+ */
+module.exports = {
+  appId: 'com.littlebit.launcher',
+  productName: 'Little Bit',
+  copyright: 'Copyright © 2024 Little Bit UA',
+
+  directories: {
+    output: 'release/${version}',
+  },
+
+  files: ['out/**/*'],
+
+  asarUnpack: ['**/*.node'],
+
+  compression: 'store',
+
+  publish: {
+    provider: 'github',
+    owner: 'Vadko',
+    repo: 'littlebit-launcher',
+  },
+
+  win: {
+    target: [
+      {
+        target: 'nsis',
+        arch: ['x64'],
+      },
+      {
+        target: 'portable',
+        arch: ['x64'],
+      },
+    ],
+    publisherName: 'Little Bit UA',
+    verifyUpdateCodeSignature: false,
+  },
+
+  linux: {
+    target: ['AppImage', 'deb'],
+    category: 'Utility',
+    maintainer: 'Little Bit UA <info@littlebit.org.ua>',
+  },
+
+  nsis: {
+    oneClick: true,
+    perMachine: false,
+    allowToChangeInstallationDirectory: false,
+    deleteAppDataOnUninstall: false,
+    differentialPackage: true,
+  },
+
+  electronDist: 'node_modules/electron/dist',
+  electronVersion: '28.0.0',
+};
